966888ea640dea7474695db8789a9fa6f12ec76d
soma-maruuchi <mwa60346@gmail.com>
2020-10-13 13:02:56 +0900

    environmentの入れ替え

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,5 @@
-const { environment } = require('@rails/webpacker')
+# Load the Rails application.
+require_relative 'application'
 
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-}))
-
-module.exports = environment
+# Initialize the Rails application.
+Rails.application.initialize!
