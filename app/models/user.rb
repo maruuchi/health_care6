d9bd717aec6c6868f17d0d009382c20f00fab7d4
soma-maruuchi <mwa60346@gmail.com>
2020-10-17 23:02:36 +0900

    モデルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+                    # destroy時に関連づけられたモデルに対してdestroyが実行される
+  has_many :graphs, dependent: :destroy
 end
