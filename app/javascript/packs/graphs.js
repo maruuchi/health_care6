d406a32951abfdc76e57d5fe719d5378dacb8692
soma-maruuchi <mwa60346@gmail.com>
2020-10-19 22:42:16 +0900

    グラフ更新に対応するための調整

--- app/javascript/packs/graphs.js
+++ app/javascript/packs/graphs.js
@@ -12,6 +12,8 @@ document.addEventListener('turbolinks:load', () => {
   // グラフを描く場所を取得
   const chartWeightContext = document.getElementById("chart-weight").getContext('2d')
 
+  let chartWeight
+
   // 期間を指定してグラフを描く
   const drawGraph = (from, to) => {
     // from から to までの期間のデータに絞る
@@ -56,11 +58,19 @@ document.addEventListener('turbolinks:load', () => {
       }
     }
 
-    new Chart(chartWeightContext, {
-      type: 'line',
-      data: weightData,
-      options: weightOption
-    })
+    if (!chartWeight) {
+      // グラフが存在しないときは，作成する
+      chartWeight = new Chart(chartWeightContext, {
+        type: 'line',
+        data: weightData,
+        options: weightOption
+      })
+    } else {
+      // グラフが存在するときは，更新する
+      chartWeight.data = weightData
+      chartWeight.options = weightOption
+      chartWeight.update()
+    }
   }
 
   // グラフの初期表示
