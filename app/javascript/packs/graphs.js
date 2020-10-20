6d8f409c69fb70e10ecbf1e1d3c14c61f522ab27
soma-maruuchi <mwa60346@gmail.com>
2020-10-21 07:52:28 +0900

    モーダル内のカレンダーにてflatpickrを使用可能にする

--- app/javascript/packs/graphs.js
+++ app/javascript/packs/graphs.js
@@ -1,5 +1,7 @@
 document.addEventListener('turbolinks:load', () => {
   
+  const convertDate = (date) => new Date(new Date(date).setHours(0, 0, 0, 0))
+
   const minDate = (date1, date2) => (date1 < date2) ? date1 : date2
   const maxDate = (date1, date2) => (date1 > date2) ? date1 : date2
 
@@ -35,7 +37,15 @@ document.addEventListener('turbolinks:load', () => {
   const startCalendarFlatpickr = flatpickr('#start-calendar', periodCalendarOption)
   const endCalendarFlatpickr = flatpickr('#end-calendar', periodCalendarOption)
 
-  const convertDate = (date) => new Date(new Date(date).setHours(0, 0, 0, 0))
+
+  flatpickr('#new-calendar', {
+    disableMobile: true,
+    // 記録のある日付を選択できないようにする
+    disable: gon.recorded_dates,
+    defaultDate: 'today',
+  })
+
+  
 
   const TODAY = convertDate(new Date())
   const A_WEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 6)
