7dcf2af5abc034e1a9f91b7b6eaf4787b669fc97
soma-maruuchi <mwa60346@gmail.com>
2020-10-21 07:18:06 +0900

    新規登録用のモーダル作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'graphs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :graphs, only: %i[index create update]
 end
