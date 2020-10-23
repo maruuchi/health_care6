195c1e3d01d3a93cf98c6c5fa1396b80758ccf14
soma-maruuchi <mwa60346@gmail.com>
2020-10-23 17:11:38 +0900

    ゲストユーザー機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+
   root 'graphs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :graphs, only: %i[index create update]
+
   devise_scope :user do
-    post '/users/guest_sign_in', to: 'users#new_guest'
+    post '/users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 end
